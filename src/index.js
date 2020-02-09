e8ceca3d3f6ff1ebc833882e82f5b1ccf3a68ccc
huystart17 <huy.start17@gmail.com>
2020-02-10 03:54:46 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
+import {html} from "./view/home";
+
 const Koa = require('koa');
 const app = new Koa();
 
-// Make Mithril happy
-if (!global.window) {
-  global.window = global.document = global.requestAnimationFrame = undefined
-}
-
-var m = require('mithril')
-var render = require('mithril-node-render')
-
-render(m('span', 'huhu')).then(function (html) {
-  // html === '<span>huhu</span>'
-})
-
-var html = render.sync(m('h1', 'huhu'))
-// html === '<span>huhu</span>'
-
-
 app.use(async ctx => {
-  ctx.body = html;
+    ctx.body= html()
 });
 
 app.listen(3000);
--- src/view/home.js
+++ src/view/home.js
@@ -1,14 +1,11 @@
+//type = module
 // Make Mithril happy
 if (!global.window) {
-  global.window = global.document = global.requestAnimationFrame = undefined
+    global.window = global.document = global.requestAnimationFrame = undefined;
 }
 
-var m = require('mithril')
-var render = require('mithril-node-render')
+var m = require('mithril');
+var render = require('mithril-node-render');
 
-render(m('span', 'huhu')).then(function (html) {
-  // html === '<span>huhu</span>'
-})
-
-var html = render.sync(m('h1', 'huhu'))
+export const html = render.sync(m('h1', 'huhu'));
 // html === '<span>huhu</span>'
